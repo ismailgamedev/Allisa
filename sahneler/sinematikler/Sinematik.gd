1898d4a54fd293158bf9909103c44ef6a60d6890
Ismail Filipov <64804972+ismailgamedev@users.noreply.github.com>
2022-02-18 23:51:53 +0100

    sinematik

--- sahneler/sinematikler/Sinematik.gd
+++ sahneler/sinematikler/Sinematik.gd
@@ -1,18 +1,17 @@
 extends Control
 
-export(String, FILE, "*.tscn") var diger_sahne_path
-export(NodePath) var cizim_node_path
+export(String) var diger_sahne_path
+export(StreamTexture) var cizim_node_path
 
 onready var sahnegecisi = $CanvasLayer/Sahnegecisi
 
-var diger_sahne
-var cizim
+onready var cizim = get_node("TextureRect")
 
 func _ready():
-	cizim = get_node(cizim_node_path)
-	diger_sahne = get_node(diger_sahne_path)
+	cizim.texture = cizim_node_path
 
 func _process(delta):
 	if Input.is_action_just_pressed("diger_sinematik"):
-		sahnegecisi.sahneye_git(diger_sahne)
+		print(str(diger_sahne_path))
+		sahnegecisi.sahneye_git(diger_sahne_path)
 
