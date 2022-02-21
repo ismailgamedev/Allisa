f1210fc2d90c97d50d33b9b92ebf05111a01def0
beetlejuicetr <keremdemir7526@gmail.com>
2022-02-21 18:52:34 +0300

    bug fixed

--- sahneler/oyun/mezar.gd
+++ sahneler/oyun/mezar.gd
@@ -2,23 +2,25 @@ extends Area2D
 
 var verdi = false
 var obj
-
+var aldi = false
 func _process(delta):
 	if verdi == true:
-		if Input.is_action_just_pressed("etkilesim"):
-			obj.global_position = Vector2(548,111)
-			yield(get_tree().create_timer(0.2),"timeout")
-			obj.visible = false
-			obj.hareket_izin = false
-			$AnimationPlayer.play("sev")
-			yield($AnimationPlayer,"animation_finished")
-			yield(get_tree().create_timer(0.8),"timeout")
-			obj.visible = true
-			obj.hareket_izin = true
-			$Mezar.frame = 1
-			$AnimationPlayer.play("idle")
-			get_parent().get_node("yeni_seviye_kapisi").gecis_izni = true
-			
+		if aldi == false:
+			if Input.is_action_just_pressed("etkilesim"):
+				obj.global_position = Vector2(548,111)
+				yield(get_tree().create_timer(0.2),"timeout")
+				obj.visible = false
+				obj.hareket_izin = false
+				$AnimationPlayer.play("sev")
+				yield($AnimationPlayer,"animation_finished")
+				yield(get_tree().create_timer(0.8),"timeout")
+				obj.visible = true
+				obj.hareket_izin = true
+				$Mezar.frame = 1
+				$AnimationPlayer.play("idle")
+				get_parent().get_node("yeni_seviye_kapisi").gecis_izni = true
+				aldi = true
+				$Label.visible = false
 	else:
 		pass
 
@@ -26,9 +28,17 @@ func _process(delta):
 
 
 func _on_mezar_body_entered(body):
+	if aldi == false:
+		$Label.visible = true
 	verdi = true
 	obj = body
 
 
 func _on_mezar_body_exited(body):
 	verdi = false
+	$Label.visible = false
+
+
+func _ready():
+	$Label.visible = false
+	pass
--- ui/HakkimdaMenu.gd
+++ ui/HakkimdaMenu.gd
@@ -6,3 +6,9 @@ onready var sahnegecisi = $CanvasLayer/Sahnegecisi
 
 func _on_Button_pressed():
 	sahnegecisi.sahneye_git("res://ui/MainMenu.tscn")
+
+
+func _ready():
+	#$BG.rect_global_position = Vector2(1024,600)
+	$BG.rect_size = Vector2(1024,600)
+	pass
